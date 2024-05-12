71ef0c81654234af8f1cc5742169441d21221c89
emma-caldwell <169080098+emma-caldwell@users.noreply.github.com>
2024-05-12 12:42:56 +0000

    removed limit from select order sum

--- Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
+++ Chapter 2/Using Data Types/SelectCustomerOrdersSum.sql
@@ -6,6 +6,4 @@ From Customer
   JOIN Orders on Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
   FirstName,
-  LastName
-ORDER BY Sum(TotalDue) DESC
-LIMIT 10 -- Click on Run on active connection to run
+  LastName; -- Click on Run on active connection to run
